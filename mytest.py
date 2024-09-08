12ed79396ffb99471bb48bc7f21fda605583bb75
fazzy01 <famaths01@gmail.com>
2024-09-08 16:21:17 -0700

    first commit on continous integration with pytest v02

--- mytest.py
+++ mytest.py
@@ -5,6 +5,6 @@ def inc(x):
     return x + 1
 
 
-# it must start with test before pytest can see it.
+# it must start with test before pytest can see it..
 def test_answer():
     assert inc(3) == 5
